e79f73822379d21a3b6f11004597e28d9ac10b74
ldm <401260829@qq.com>
2023-06-24 17:50:14 +0800

    2

--- oss-s3-starter-autoconfiguration/src/main/java/com/ldm/hello/auto/OwsAutoConfiguration.java
+++ oss-s3-starter-autoconfiguration/src/main/java/com/ldm/hello/auto/OwsAutoConfiguration.java
@@ -19,6 +19,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * 自动装配类
+ */
 @Configuration
 @EnableConfigurationProperties(OssProperties.class)
 @ConditionalOnClass(OwsTemplate.class)
